e05b6be020cb040903e75bd0252044f9035042cf
Lucas Garboza <mouktae@live.com>
2025-01-17 13:54:40 -0300

    Atualização 1.2

--- Banco_de_Dados1_2/IntegraçaoSQL.py
+++ Banco_de_Dados1_2/IntegraçaoSQL.py
@@ -2,8 +2,9 @@ import sqlite3
 from sqlite3 import Error
 import os
 
+# Função que tentar integrar o Python com o SQLite e retorna conexão com o banco de dados
 def ConexaoSql():
-    local = "C:\\Users\\BLAST INFO E TECH\\Desktop\\Mouk-SQL\\Usuarios Cadastrados.db"
+    local = "C:\\Users\\BLAST INFO E TECH\\Desktop\\Mouk-SQL\\Mouk-SQL\\Banco_de_Dados\\Banco_de_Dados1_2\\Usuarios Cadastrados.db"
     conexao = None
     try:
         conexao = sqlite3.connect(local)
@@ -11,3 +12,4 @@ def ConexaoSql():
         print(erro)
     finally:
         return conexao
+
